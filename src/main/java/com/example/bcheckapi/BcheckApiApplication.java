67116bec23e6968ddc7ec1b39bf5511a04384822
hanbee <sonhanbi1002@gmail.com>
2021-09-30 13:14:50 +0900

    init commit

---
+++ src/main/java/com/example/bcheckapi/BcheckApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bcheckapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcheckApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcheckApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bcheckapi/BcheckApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bcheckapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcheckApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
