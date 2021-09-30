80587bbe437d1ca9e3aefb487dc0cb1d18457458
hanbee <sonhanbi1002@gmail.com>
2021-09-30 14:42:32 +0900

    Swagger 연결하기

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # bCheck API
 사내 도서 관리 시스템 api 프로젝트입니다.
+
+## Swagger 3.0 연결
+- build.gradle 파일에 라이브러리 추가
+```
+...
+
+ext {
+    swaggerVersion = '3.0.0'
+    openApiVersion = '1.5.8'
+}
+
+...
+
+dependencies {
+    ...
+    
+    // swagger
+    implementation "io.springfox:springfox-swagger2:${swaggerVersion}"
+    implementation "io.springfox:springfox-swagger-ui:${swaggerVersion}"
+    implementation "org.springdoc:springdoc-openapi-ui:${openApiVersion}"
+    
+    ...
+}
+```
+- SwaggerConfig.java 파일 추가 (설정 정보 변경)
+- 서버 실행 후 http://localhost:9090/app/swagger-ui.html 접속
---
+++ src/main/java/com/example/bcheckapi/config/SwaggerConfig.java
@@ -0,0 +1,56 @@
+package com.example.bcheckapi.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "bCheck API",
+                version = "v1",
+                description = "ref - bCheck-API",
+                contact = @Contact(name = "Hanbee Son",
+                        email = "sonhanbi1002@gmail.com",
+                        url = "www.example.com"
+                )
+        ),
+        servers = {
+                @Server(url = "http://localhost:9090/app", description = "local-server"),
+        },
+        security = {
+                @SecurityRequirement(name = "bearerToken"),
+        }
+)
+
+@SecuritySchemes({
+        @SecurityScheme(
+                name = "bearerToken",
+                type = SecuritySchemeType.HTTP,
+                scheme = "bearer",
+                bearerFormat = "JWT"
+        ),
+})
+@Configuration
+public class SwaggerConfig {
+    String[] loginPaths = {"/auth/**"};
+    String[] allPaths = {"/**"};
+
+    @Bean
+    public GroupedOpenApi authOpenApi() {
+        return GroupedOpenApi.builder().group("auth-API").pathsToMatch(loginPaths).build();
+    }
+
+    @Bean
+    public GroupedOpenApi allOpenApi() {
+        return GroupedOpenApi.builder().group("all-API").pathsToMatch(allPaths).build();
+    }
+
+}
