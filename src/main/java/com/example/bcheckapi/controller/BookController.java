63d9e3029acbeb884d844a7b68016c81c491cf16
hanbee <sonhanbi1002@gmail.com>
2021-12-01 17:58:35 +0900

    BookController 주석 처리 (임시)

--- src/main/java/com/example/bcheckapi/controller/BookController.java
+++ src/main/java/com/example/bcheckapi/controller/BookController.java
@@ -1,16 +1,7 @@
 package com.example.bcheckapi.controller;
 
-import com.example.bcheckapi.dto.*;
-import com.example.bcheckapi.service.BookService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "[001] 도서 관리(도서 조회, 추가, 수정, 삭제)", description = "도서 조회, 추가, 수정, 삭제")
@@ -18,47 +9,47 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/books")
 @AllArgsConstructor
 public class BookController {
-
-    final BookService bookService;
-
-    @Operation(description = "도서 조회 (도서명, ISBN 검색)")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "검색된 도서 리스트를 반환한다.",
-                    content = @Content(schema = @Schema(implementation = BookSearchResponse.class)))
-    })
-    @GetMapping
-    public ResponseEntity<?> searchBooks(@Parameter(description = "도서명 or ISBN", example="토비") @RequestParam(required = false) String word) {
-        return ResponseEntity.ok(bookService.searchBookList(word));
-    }
-
-    @Operation(description = "도서 추가")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "도서 추가 후 결과를 반환한다.",
-                    content = @Content(schema = @Schema(implementation = GeneralResponse.class)))
-    })
-    @PostMapping
-    public ResponseEntity<?> addBook(@RequestBody BookRegisterRequest request) {
-        return ResponseEntity.ok(bookService.registerBook(request));
-    }
-
-    @Operation(description = "도서 소유자 수정")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "도서 소유자 수정 후 결과를 반환한다.",
-                    content = @Content(schema = @Schema(implementation = GeneralResponse.class)))
-    })
-    @PatchMapping
-    public ResponseEntity<?> changeBookOwner(@RequestBody BookOwnerChangeRequest request) {
-        return ResponseEntity.ok(bookService.changeBookOwner(request));
-    }
-
-    @Operation(description = "도서 삭제 (삭제 여부 수정)")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "도서 삭제 후 결과를 반환한다.",
-                    content = @Content(schema = @Schema(implementation = GeneralResponse.class)))
-    })
-    @DeleteMapping
-    public ResponseEntity<?> removeBook(@RequestBody BookRemoveRequest request) {
-        return ResponseEntity.ok(bookService.removeBook(request));
-    }
+//
+//    final BookService bookService;
+//
+//    @Operation(description = "도서 조회 (도서명, ISBN 검색)")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "검색된 도서 리스트를 반환한다.",
+//                    content = @Content(schema = @Schema(implementation = BookSearchResponse.class)))
+//    })
+//    @GetMapping
+//    public ResponseEntity<?> searchBooks(@Parameter(description = "도서명 or ISBN", example="토비") @RequestParam(required = false) String word) {
+//        return ResponseEntity.ok(bookService.searchBookList(word));
+//    }
+//
+//    @Operation(description = "도서 추가")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "도서 추가 후 결과를 반환한다.",
+//                    content = @Content(schema = @Schema(implementation = GeneralResponse.class)))
+//    })
+//    @PostMapping
+//    public ResponseEntity<?> addBook(@RequestBody BookRegisterRequest request) {
+//        return ResponseEntity.ok(bookService.registerBook(request));
+//    }
+//
+//    @Operation(description = "도서 소유자 수정")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "도서 소유자 수정 후 결과를 반환한다.",
+//                    content = @Content(schema = @Schema(implementation = GeneralResponse.class)))
+//    })
+//    @PatchMapping
+//    public ResponseEntity<?> changeBookOwner(@RequestBody BookOwnerChangeRequest request) {
+//        return ResponseEntity.ok(bookService.changeBookOwner(request));
+//    }
+//
+//    @Operation(description = "도서 삭제 (삭제 여부 수정)")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "도서 삭제 후 결과를 반환한다.",
+//                    content = @Content(schema = @Schema(implementation = GeneralResponse.class)))
+//    })
+//    @DeleteMapping
+//    public ResponseEntity<?> removeBook(@RequestBody BookRemoveRequest request) {
+//        return ResponseEntity.ok(bookService.removeBook(request));
+//    }
 
 }
